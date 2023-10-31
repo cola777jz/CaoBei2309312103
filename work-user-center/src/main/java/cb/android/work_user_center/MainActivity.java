78a6ad2424939a1ce09b00a5d56190cda8da9f31
cola777jz <cola777jz@gmail.com>
2023-10-31 16:11:12 +0800

    work-user-center

---
+++ work-user-center/src/main/java/cb/android/work_user_center/MainActivity.java
@@ -0,0 +1,46 @@
+package cb.android.work_user_center;
+
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private RelativeLayout rlOrder, rlAddress, rlLogout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        rlOrder = findViewById(R.id.rl_order);
+        rlAddress = findViewById(R.id.rl_address);
+        rlLogout = findViewById(R.id.rl_logout);
+
+        rlOrder.setOnClickListener(view -> {
+            dialog("下一步将查询个人订单");
+        });
+        rlAddress.setOnClickListener(view -> {
+            dialog("下一步将编辑个人地址");
+        });
+        rlLogout.setOnClickListener(view -> {
+            dialog("下一步将退出登录");
+        });
+    }
+
+    public void login(View view) {
+        dialog("下一步将跳转到登陆页面");
+    }
+
+
+    private void dialog(String msg) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder
+                .setMessage(msg)
+                .setPositiveButton("确定", (dialogInterface, i) -> {
+                    Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+                }).create().show();
+    }
+}
