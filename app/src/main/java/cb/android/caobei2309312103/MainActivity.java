8027bbdc17483c2257598086345ba736f97392cb
cola777jz <cola777jz@gmail.com>
2023-10-31 13:32:21 +0800

    init commit

---
+++ app/src/main/java/cb/android/caobei2309312103/MainActivity.java
@@ -0,0 +1,13 @@
+package cb.android.caobei2309312103;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
