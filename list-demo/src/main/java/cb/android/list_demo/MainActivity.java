015ceb5496859b2f3f14b4c7ce59ae1c1726a4c7
cola777jz <cola777jz@gmail.com>
2023-10-31 17:26:14 +0800

    list-demo

--- list-demo/src/main/java/cb/android/list_demo/MainActivity.java
+++ list-demo/src/main/java/cb/android/list_demo/MainActivity.java
@@ -1,5 +1,9 @@
 package cb.android.list_demo;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_spinner = findViewById(R.id.main_btn_spinner);
+        Button btn_list_view = findViewById(R.id.main_btn_listView);
+        Button btn_grid_view  = findViewById(R.id.main_btn_gridview);
+        Button btn_recycle_view = findViewById(R.id.main_btn_recyclerview);
+
+        btn_spinner.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SpinnerActivity.class);
+            startActivity(intent);
+        });
+
+        btn_list_view.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, ListActivity.class);
+            startActivity(intent);
+        });
+
+        btn_grid_view.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, GridActivity.class);
+            startActivity(intent);
+        });
+
+        btn_recycle_view.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, RecycleActivity.class);
+            startActivity(intent);
+        });
     }
 }
